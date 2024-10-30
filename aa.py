7bde7f92af298957e9b9cd1a040de355720a8bb5
Allan Cristian Krause <allan.krause09@hotmail.com>
2024-10-29 21:48:57 -0400

    final aula

--- aa.py
+++ aa.py
@@ -1,6 +1,6 @@
 alunos = {}
 
-def inserirAlterar():
+def inserirAluno():
     nome = input("Insira o nome do aluno: ")
     nota = int(input("Insira a nota do aluno: "))
     if nome in alunos:
@@ -9,11 +9,28 @@ def inserirAlterar():
         print(f"Foi adicionado o aluno {nome} com a nota {nota}")
     alunos[nome] = nota
 
+def alterarAluno():
+    nome = input("Insira o nome do aluno: ")
+    if nome in alunos:
+        nota = int(input("Insira a nota do aluno: "))
+        print(f"Foi alterado o aluno {nome} pela nota {nota}")
+        alunos[nome] = nota
+    else:
+        print(f"Não encontrado o aluno solicitado")
+    
+def excluirAluno():
+    nome = input("Insira o nome do aluno: ")
+    if nome in alunos:
+        alunos.pop(nome)
+        print(f"Apagado o aluno {nome}")
+    else:
+        print("Aluno não encontrado")
+
 while True:
     opcao = int(input("selecione a opção desejada: \n 1 - Adicionar aluno\n 2- Remover aluno\n 3 - Alterar nota\n 4 - Consultar Aluno\n 5 - Consultar Media\n 6 - Encerrar\n"))
     match opcao: 
         case 1:
-            inserirAlterar()
+            inserirAluno()
         case 2:
             print("Remover aluno")
             nome = input("Digite o aluno a ser removido")
@@ -22,12 +39,7 @@ while True:
             else:
                 print("Aluno não encontrado")
         case 3:
-            print("Alterar nota:")
-            nome =  input("Digite o nome do aluno: ")
-            if nome in alunos:
-                alunos[nome] = int(input("Digite a nota do aluno: "))
-            else:
-                print("Aluno não encontrado")
+            alterarAluno()
         case 4:
             print(alunos)
         case 5:
