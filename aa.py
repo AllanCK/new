bff7c72cc23babfc9a8eb36bdecd23d41cc7e7e1
Allan Cristian Krause <allan.krause09@hotmail.com>
2024-11-04 11:19:23 -0400

    Quase final

--- aa.py
+++ aa.py
@@ -1,50 +1,91 @@
-alunos = {}
+alunos = {"allan": 5, "Ana": 5}
+    
+def separador():
+    print("\n ------------------------------- \n")
 
 def inserirAluno():
-    nome = input("Insira o nome do aluno: ")
-    nota = int(input("Insira a nota do aluno: "))
-    if nome in alunos:
-        print(f"Foi alterado o aluno {nome} pela nota {nota}")
-    else:
-        print(f"Foi adicionado o aluno {nome} com a nota {nota}")
-    alunos[nome] = nota
-
+    while True:
+        separador()
+        nome = input("Insira o nome do aluno: ").capitalize()
+        while True:
+            try:
+                nota = int(input("Insira a nota do aluno: "))
+            except:
+                print("Insira um numero para a nota")
+                continue
+            else:
+                if nome in alunos:
+                    print(f"Foi alterado a nota do aluno {nome} pela nota {nota}")
+                else:
+                    print(f"Foi adicionado o aluno {nome} com a nota {nota}")
+                alunos[nome] = nota
+                break
+        resposta = input("Digite 1 para voltar ao menu inicial ou qualquer tecla para continuar  ")
+        if resposta == "1":
+            break
+        
 def alterarAluno():
-    nome = input("Insira o nome do aluno: ")
-    if nome in alunos:
-        nota = int(input("Insira a nota do aluno: "))
-        print(f"Foi alterado o aluno {nome} pela nota {nota}")
-        alunos[nome] = nota
-    else:
-        print(f"Não encontrado o aluno solicitado")
-    
+    while True:
+        separador()
+        nome = input("Insira o nome do aluno: ")
+        if nome in alunos:
+            while True:
+                try:
+                    nota = int(input("Insira a nota do aluno: "))
+                except:
+                    print("digite apenas numeros")
+                    continue
+                else:
+                    print(f"Foi alterado a nota do aluno {nome} pela nota {nota}")
+                    alunos[nome] = nota
+                    break
+        else:
+            print(f"Não encontrado o aluno solicitado")
+        resposta = input("Digite 1 para voltar ao menu inicial ou qualquer tecla para continuar  ")
+        if resposta == "1":
+            break
+
 def excluirAluno():
-    nome = input("Insira o nome do aluno: ")
-    if nome in alunos:
-        alunos.pop(nome)
-        print(f"Apagado o aluno {nome}")
-    else:
-        print("Aluno não encontrado")
+    while True:
+        separador()
+        nome = input("Insira o nome do aluno: ")
+        if nome in alunos:
+            alunos.pop(nome)
+            print(f"Apagado o aluno {nome}")
+        else:
+            print("Aluno não encontrado")
+        resposta = input("Digite 1 para voltar ao menu inicial ou qualquer tecla para continuar  ")
+        if resposta == "1":
+            break
+
+def consultarAlunos():
+    separador()
+    print("NOME      NOTA")
+    for aluno, nota in alunos.items():
+        print(f"{aluno}      {nota}")
+    input("\nDigite qualquer tecla para retornar ao menu principal  ")
+
+def mediaNotas():
+    separador()
+    media = sum(alunos.values()) / len(alunos)
+    print(media)  
+    input("\nDigite qualquer tecla para retornar ao menu principal  ")      
 
 while True:
-    opcao = int(input("selecione a opção desejada: \n 1 - Adicionar aluno\n 2- Remover aluno\n 3 - Alterar nota\n 4 - Consultar Aluno\n 5 - Consultar Media\n 6 - Encerrar\n"))
-    match opcao: 
+    separador()
+    opcao = int(input("selecione a opção desejada: \n\n  1 - Adicionar aluno\n  2- Remover aluno\n  3 - Alterar nota\n  4 - Consultar Aluno\n  5 - Consultar Media\n  6 - Encerrar\n\n"))
+    match opcao:  
         case 1:
             inserirAluno()
         case 2:
-            print("Remover aluno")
-            nome = input("Digite o aluno a ser removido")
-            if nome in alunos:
-                alunos.pop(nome)
-            else:
-                print("Aluno não encontrado")
+            excluirAluno()
         case 3:
             alterarAluno()
         case 4:
-            print(alunos)
+            consultarAlunos()
         case 5:
-            notas = alunos.values()
-            print("Media de notas:")
-            print(f"A media de notas ficou em {notas}")
-        
-
+            mediaNotas()
+        case 6:
+            break
+        case _:
+            print("\nDigite uma opção valida")
